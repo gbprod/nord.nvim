0d23a34bd00268a1f745e73524862aa010678299
gbprod <contact@gb-prod.fr>
2022-12-05 15:47:47 +0100

    feat: support p00f/nvim-ts-rainbow

--- lua/nord/plugins/ui.lua
+++ lua/nord/plugins/ui.lua
@@ -99,9 +99,14 @@ function ui.highlights()
     AerialTypeParameter = { fg = c.frost.artic_ocean },
     AerialVariable = { fg = c.snow_storm.origin, bold = true },
 
-    -- vigoux/notifier.nvim
-    NotifierTitle = { fg = c.frost.polar_water, bold = true },
-    NotifierContentDim = { fg = c.polar_night.light },
+    -- p00f/nvim-ts-rainbow
+    rainbowcol1 = { fg = c.frost.artic_water },
+    rainbowcol2 = { fg = c.aurora.green },
+    rainbowcol3 = { fg = c.aurora.red },
+    rainbowcol4 = { fg = c.aurora.yellow },
+    rainbowcol5 = { fg = c.frost.polar_water },
+    rainbowcol6 = { fg = c.aurora.purple },
+    rainbowcol7 = { fg = c.snow_storm.origin },
   }
 end
 
