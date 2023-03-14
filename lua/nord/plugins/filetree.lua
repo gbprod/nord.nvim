bc077cc78a339d44f41d9ceb49dd22267af1c521
gbprod <contact@gb-prod.fr>
2023-03-14 14:47:45 +0100

    feat: improve neo-tree support

--- lua/nord/plugins/filetree.lua
+++ lua/nord/plugins/filetree.lua
@@ -23,17 +23,26 @@ function filetree.highlights()
     NvimTreeFolderIcon = { fg = c.frost.artic_water },
     NvimTreeOpenedFolderName = { fg = c.snow_storm.origin },
 
-    NeoTreeSymbolicLinkTarget = { fg = c.frost.artic_water },
-    NeoTreeRootName = { fg = c.snow_storm.origin, bold = true },
-    NeoTreeDirectoryName = { fg = c.snow_storm.origin },
+    NeoTreeDimText = { fg = c.polar_night.brightest },
     NeoTreeDirectoryIcon = { fg = c.frost.artic_water },
-    NeoTreeFileName = { fg = c.snow_storm.origin },
+    NeoTreeDirectoryName = { fg = c.snow_storm.origin },
+    NeoTreeDotfile = { fg = c.polar_night.brightest },
+    NeoTreeFadeText1 = { fg = c.polar_night.brightest },
+    NeoTreeFadeText2 = { fg = c.polar_night.brighter },
     NeoTreeFileIcon = { fg = c.snow_storm.origin },
-
-    NeoTreeTabInactive = { fg = c.polar_night.light, bg = global_bg },
+    NeoTreeFileName = { fg = c.snow_storm.origin },
+    NeoTreeGitUnstaged = { fg = c.aurora.orange, italic = true },
+    NeoTreeGitUntracked = { fg = c.aurora.green, italic = true },
+    NeoTreeGitConflict = { fg = c.aurora.red },
+    NeoTreeIndentMarker = { fg = c.polar_night.brighter },
+    NeoTreeMessage = { fg = c.polar_night.brightest, italic = true },
+    NeoTreeModified = { fg = c.aurora.yellow },
+    NeoTreeRootName = { fg = c.snow_storm.origin, bold = true },
+    NeoTreeSymbolicLinkTarget = { fg = c.frost.artic_water },
     NeoTreeTabActive = { fg = c.snow_storm.origin, bg = c.polar_night.brighter, bold = true },
-    NeoTreeTabSeparatorInactive = { fg = c.polar_night.origin, bg = global_bg },
+    NeoTreeTabInactive = { fg = c.polar_night.light, bg = global_bg },
     NeoTreeTabSeparatorActive = { fg = c.polar_night.brighter, bg = global_bg },
+    NeoTreeTabSeparatorInactive = { fg = c.polar_night.origin, bg = global_bg },
   }
 end
 
