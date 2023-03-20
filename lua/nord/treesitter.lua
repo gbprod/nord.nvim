bd32c0f4d61c4a49a5ea8c2829f370a52fe1bcdd
Егор Мартынов <48406064+mrtnvgr@users.noreply.github.com>
2023-03-20 15:00:42 +0700

    feat: add support for semantic token modifiers (#29)
    * feat: add support for semantic token modifiers
    
    * `@lsp.type.interface`: `Identifier` -> `@type`

--- lua/nord/treesitter.lua
+++ lua/nord/treesitter.lua
@@ -109,6 +109,19 @@ function treesitter.highlights()
     ["@field.yaml"] = { fg = c.frost.polar_water },
     ["@text.phpdoc"] = { fg = c.polar_night.light },
     ["@attribute.phpdoc"] = { fg = c.frost.artic_water },
+
+    -- LSP Semantic Token Groups
+    ["@lsp.type.comment"] = { link = "@comment" },
+    ["@lsp.type.enum"] = { link = "@type" },
+    ["@lsp.type.interface"] = { link = "@type" },
+    ["@lsp.type.keyword"] = { link = "@keyword" },
+    ["@lsp.type.namespace"] = { link = "@namespace" },
+    ["@lsp.type.parameter"] = { link = "@parameter" },
+    ["@lsp.type.property"] = { link = "@property" },
+    ["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
+    ["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
+    ["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
+    ["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
   }
 end
 
