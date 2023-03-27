e7d7135a1c5034deea72a5d8d42a25fdb93c85c1
Jiahe Jiang <61075605+Isrothy@users.noreply.github.com>
2023-03-27 17:51:26 +0800

    BREAKING CHANGE!: sync aerial & navic highlights with treesitter & lsp highlights (#30)
    * BREAKING CHANGE!: sync aerial & navic highlights with treesitter & lsp highlights
    
    * add semantic token default value
    
    * fix: typo

--- lua/nord/treesitter.lua
+++ lua/nord/treesitter.lua
@@ -111,13 +111,20 @@ function treesitter.highlights()
     ["@attribute.phpdoc"] = { fg = c.frost.artic_water },
 
     -- LSP Semantic Token Groups
+    ["@lsp.type.class"] = { link = "@type" },
     ["@lsp.type.comment"] = { link = "@comment" },
     ["@lsp.type.enum"] = { link = "@type" },
+    ["@lsp.type.enumMember"] = { link = "@constant" },
+    ["@lsp.type.field"] = { link = "@field" },
+    ["@lsp.type.function"] = { link = "@function" },
     ["@lsp.type.interface"] = { link = "@type" },
     ["@lsp.type.keyword"] = { link = "@keyword" },
+    ["@lsp.type.method"] = { link = "@method" },
     ["@lsp.type.namespace"] = { link = "@namespace" },
     ["@lsp.type.parameter"] = { link = "@parameter" },
     ["@lsp.type.property"] = { link = "@property" },
+    ["@lsp.type.struct"] = { link = "@structure" },
+    ["@lsp.type.typeParameter"] = { link = "@parameter" },
     ["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
     ["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
     ["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
