25f45989fc320ae8948335704dc9c22c8a0af0c9
gbprod <contact@gb-prod.fr>
2023-07-18 09:35:36 +0200

    fix: differenciate LeapMatch from LeapLabelPrimary
    Fixes: #38

--- lua/nord/plugins/motion.lua
+++ lua/nord/plugins/motion.lua
@@ -4,7 +4,7 @@ local c = require("nord.colors").palette
 
 function motion.highlights()
   return {
-    LeapMatch = { bg = c.aurora.green, fg = c.polar_night.bright, bold = true, nocombine = true },
+    LeapMatch = { fg = c.polar_night.bright, bg = c.frost.polar_water, bold = true, nocombine = true },
     LeapLabelPrimary = { fg = c.polar_night.bright, bg = c.aurora.green, bold = true, nocombine = true },
     LeapLabelSecondary = { fg = c.polar_night.bright, bg = c.aurora.purple, bold = true, nocombine = true },
     LeapLabelSelected = { fg = c.polar_night.bright, bg = c.aurora.yellow, bold = true, nocombine = true },
