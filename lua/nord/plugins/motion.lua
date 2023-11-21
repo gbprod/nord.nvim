e634f50e6e3ba8810667e2eac1c1a9d1df22e4ca
gbprod <contact@gb-prod.fr>
2023-11-21 11:08:48 +0100

    feat: flash support

--- lua/nord/plugins/motion.lua
+++ lua/nord/plugins/motion.lua
@@ -1,6 +1,7 @@
 local motion = {}
 
 local c = require("nord.colors").palette
+local options = require("nord.config").options
 
 function motion.highlights()
   return {
@@ -8,6 +9,14 @@ function motion.highlights()
     LeapLabelPrimary = { fg = c.polar_night.bright, bg = c.aurora.green, bold = true, nocombine = true },
     LeapLabelSecondary = { fg = c.polar_night.bright, bg = c.aurora.purple, bold = true, nocombine = true },
     LeapLabelSelected = { fg = c.polar_night.bright, bg = c.aurora.yellow, bold = true, nocombine = true },
+
+    FlashLabel = { fg = c.polar_night.bright, bg = c.aurora.green, bold = true },
+    -- FlashBackdrop 	Comment 	backdrop
+    -- FlashMatch 	Search 	search matches
+    -- FlashCurrent 	IncSearch 	current match
+    -- FlashPrompt 	MsgArea 	prompt
+    -- FlashPromptIcon 	Special 	prompt icon
+    -- FlashCursor 	Cursor 	cursor
   }
 end
 
