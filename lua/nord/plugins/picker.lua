0112af6a6796103c9a3db016f3bdd499185c39a8
gbprod <contact@gb-prod.fr>
2025-01-08 13:43:47 +0100

    feat: fzf lua

--- lua/nord/plugins/picker.lua
+++ lua/nord/plugins/picker.lua
@@ -9,6 +9,19 @@ function picker.highlights()
     TelescopeTitle = { fg = c.snow_storm.origin, bold = true },
     TelescopePromptCounter = { fg = c.polar_night.light },
     TelescopeMatching = { fg = c.frost.ice, bold = true },
+
+    FzfLuaBorder = { fg = c.polar_night.light, bg = utils.make_global_bg() },
+    FzfLuaTitle = { fg = c.snow_storm.origin, bold = true },
+    FzfLuaHeaderBind = { fg = c.frost.ice },
+    FzfLuaHeaderText = { fg = c.frost.artic_ocean },
+    FzfLuaPathColNr = { fg = c.frost.ice },
+    FzfLuaPathLineNr = { fg = c.aurora.green },
+    FzfLuaBufNr = { fg = c.aurora.yellow },
+    FzfLuaBufFlagCur = { fg = c.aurora.red },
+    FzfLuaBufFlagAlt = { fg = c.frost.ice },
+    FzfLuaTabTitle = { fg = c.frost.artic_water },
+    FzfLuaTabMarker = { fg = c.aurora.yellow },
+    FzfLuaLiveSym = { fg = c.aurora.red },
   }
 end
 
